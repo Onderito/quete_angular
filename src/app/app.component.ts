f7b640f2b212f4d4c6350715e5685433cbe4d18b
Onderito <ulas.onder@outlook.fr>
2024-02-29 11:45:09 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 import { UserProfileComponent } from './user-profile/user-profile.component';
-=======
-import { BlockComponent } from './block/block.component';
->>>>>>> 9472f6243774e0d683bcfa8b6ba2ddd68342fe10
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
   imports: [RouterOutlet, UserProfileComponent],
-=======
-  imports: [RouterOutlet, BlockComponent],
->>>>>>> 9472f6243774e0d683bcfa8b6ba2ddd68342fe10
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-=======
-import { Component } from '@angular/core';
->>>>>>> 9472f6243774e0d683bcfa8b6ba2ddd68342fe10
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-<<<<<<< HEAD
   imports: [FormsModule, CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css',
@@ -29,10 +24,3 @@ export class UserProfileComponent {
     this.isShow = !this.isShow;
   }
 }
-=======
-  imports: [],
-  templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.css',
-})
-export class UserProfileComponent {}
->>>>>>> 9472f6243774e0d683bcfa8b6ba2ddd68342fe10
