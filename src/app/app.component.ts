76906c3f903ee45b88ba38adcae0e7865d636c03
Onderito <ulas.onder@outlook.fr>
2024-03-04 14:47:46 +0100

    routing finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MenuComponent,
+    SignupComponent,
+    UserProfileComponent,
+    UserComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { UserComponent } from './user/user.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'signup', component: SignupComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'userProfile', component: UserProfileComponent },
+];
