e794826ae99ff602930f9bbaaadfa325ce33262e
Onderito <ulas.onder@outlook.fr>
2024-03-07 11:59:12 +0100

    integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Ulas';
+  isNavActive: boolean = false;
+
+  onClick(): void {
+    this.isNavActive = !this.isNavActive;
+    console.log('isNavActive', this.isNavActive);
+  }
 }
